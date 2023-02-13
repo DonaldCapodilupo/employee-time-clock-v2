133385dcb2a4d03081c5cf8be075717f4ffc6cc5
Donald <59314046+DonaldCapodilupo@users.noreply.github.com>
2023-02-13 12:30:12 -0500

    Moved some functions from main.js to app.js so that I would interact with user input. If I can find a way to import the functions into app.js from main.js, that would be ideal.

--- app.js
+++ app.js
@@ -2,23 +2,157 @@
 /*jshint esversion: 6 */
 /* jshint node: true */
 
+const employees = {
+    "0000000001": "Nick",
+    "0000000002": "Keith",
+    "0000000003": "Chris",
+    "0000000004": "Dragan",
+    "0000000005": "Dave",
+    "0000000006": "Connor",
+    "0000000007": "Donald",
+    "0000000008": "Shayne",
+};
+
+let present_employees = {};
+
+function get_Current_Time() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+
+
+    let t_str = hours + ":" + minutes + ":" + seconds + " ";
+    if (hours > 11) {
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+
+    }
+
+    return t_str;
+}
+
+function get_Current_Date(){
+    let currentTime = new Date();
+    let day = currentTime.getDate();
+    let month = currentTime.getMonth();
+    let year = currentTime.getFullYear();
+
+    return month + "-" + day + "-" + year;
+
+
+}
+
+function submit_User_Input(user_input) {
+    //If user input is not a valid employee id, reject it.
+    if (employees[user_input] === undefined) {
+        console.log(user_input + " is not a valid user number");
+        return null;
+    }
+
+    //Take user input, check to see if the input matches an employee ID.
+    let employee_name = employees[user_input];
+    let current_time = get_Current_Time();
+
+    console.log(employee_name + " has punched in at " + current_time);
+
+
+    //Check to see if the employee has already been punched in.
+    if (employee_name in present_employees) {
+        console.log(employee_name + " has is being punched out.");
+        present_employees[employee_name]["Punch Out"] = current_time;
+
+    } else {
+        console.log("Logging in " + employee_name);
+        present_employees[employee_name] = {"Punch In": current_time};
+
+
+    }
+
+    create_JSON_Entry(present_employees);
+
+
+}
+
+const fs = require('fs');
+
+function create_JSON_Entry(data) {
+
+    let json_title = "./"+ get_Current_Date() + ".json";
+
+    console.log(fs.existsSync(json_title));
+    console.log(!fs.existsSync(json_title));
+
+    if (!fs.existsSync(json_title)) {
+        //create new file if not exist
+        fs.closeSync(fs.openSync(json_title, 'w'));
+
+    }
+
+    // read file
+    const file = fs.readFileSync(json_title);
+
+
+    //check if file is empty
+    if (file.length === 0) {
+        //add data to json file
+        fs.writeFileSync(json_title, JSON.stringify(data));
+    } else {
+        //append data to jso file
+        const json = [JSON.parse(file.toString())];
+        //add json element to json object
+        let new_data = data;
+        json.push(new_data);
+        fs.writeFileSync(json_title, JSON.stringify(new_data));
+    }
+}
+
 
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
 
-//console.log(__dirname +'/public');
-
+//Allows node app to access my files in the public folder. For Javascript, CSS, pictures etc.
 app.use(express.static('public'));
 
+//Needed to retrieve user input from the frontend to process on the backend.
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+
+//When the user first access the app.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'templates/index.html'));
 });
 
 
 
+//When the user makes a post request on the application.
+app.post('/', (req, res) => {
+
+    let user_input = req.body.request_input;
+
+    console.log("Input Recieved: " + user_input); // changed to body
+
+    submit_User_Input(user_input);
+
+    console.log(present_employees);
+
+
+    res.sendFile(path.join(__dirname, 'templates/index.html'), {name:"Donald"});
+});
+
+//This function runs the application.
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,8 @@
 /* use strict*/
 
 
-const employees = {
-    "0000000001": "Nick",
-    "0000000002": "Keith",
-    "0000000003": "Chris",
-    "0000000004": "Dragan",
-    "0000000005": "Dave",
-    "0000000006": "Connor",
-    "0000000007": "Donald",
-    "0000000008": "Shayne",
-};
 
-let present_employees = [];
+
 
 function get_Current_Time(){
     let currentTime = new Date();
@@ -41,52 +31,17 @@ function get_Current_Time(){
     return t_str;
 }
 
+
 function updateTime() {
     document.getElementById('time_span').innerHTML = get_Current_Time();
 
 }
 
-function punch_In_User() {
-
-    //Take user input, check to see if the input matches an employee ID.
-    let user_input = document.getElementById("employee_id").value;
-    let employee_name = employees[user_input];
-
-    //Check to see if the employee has allready been punched in.
-    if(present_employees.includes(employee_name)){
-        console.log( employee_name + " has already been logged in.");
-        return null;
-    }
 
 
-    let current_time = get_Current_Time();
-
-    //Confirmation-Div is for storing input to send to Backend
-    const confirmation_div = document.getElementById("Confirmation-Div");
-
-    //Confirmation-Prompt-Div is for popping up a message allowing the employee to know they have been punched in/out.
-    const confirmation_prompt = document.getElementById("Confirmation-Prompt-Div");
-
-    //Checks to see if user input is in the verified employees Object.
-    if (user_input && employees[user_input]) {
-        console.log("Logging in " + employees[user_input]);
-
-        present_employees.push(employee_name);
 
 
-        confirmation_div.innerHTML +=
-            '<input type="text" name="' + employee_name + '" id="" value="'+ current_time+'" hidden>';
 
-        confirmation_prompt.innerHTML +=
-            employee_name + ' has punched in at ' + current_time + '<br />';
-
-
-    } else {
-        console.log(user_input + " is not a valid user number");
-    }
-
-
-}
 
 
 function setupHTML() {
@@ -98,9 +53,12 @@ function setupHTML() {
             // Cancel the default action, if needed
             event.preventDefault();
             // Trigger the button element with a click
-            punch_In_User();
+            document.getElementById("Submit Button").click();
         }
     });
+
+
+    document.getElementById("employee_id").focus();
 }
 
 
